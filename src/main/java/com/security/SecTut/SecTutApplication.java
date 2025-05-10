a0255cfc0eee627e72a4d3b5e9ea33c13b22a4a2
Shruti-Rathore01 <shrutir8788@gmail.com>
2025-05-10 14:29:06 +0530

    Implement CSRF token functionality

---
+++ src/main/java/com/security/SecTut/SecTutApplication.java
@@ -0,0 +1,13 @@
+package com.security.SecTut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecTutApplication.class, args);
+	}
+
+}
